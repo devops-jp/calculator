efea04cfd9e7c7b6ae0a32b034749f4d95caed1b
jp <jp@darkstar.local>
2022-09-20 13:46:19 -0300

    Add hard coded delay

--- src/main/java/cl/jp/calculator/Calculator.java
+++ src/main/java/cl/jp/calculator/Calculator.java
@@ -1,9 +1,17 @@
 package cl.jp.calculator;
+
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 public class Calculator {
+	@Cacheable("sum")
 	public int sum(int a, int b) {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return a + b;
 	}
 }
